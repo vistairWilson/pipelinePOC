e222325d4fed2742cbca603b7ac087e204cabcb1
wilson.li <wilson.li@L0931.hq.vistair.com>
2019-05-28 13:48:52 +0100

    fist Commit

---
+++ src/main/java/com/pipelinePOC/PipelinePocApplication.java
@@ -0,0 +1,13 @@
+package com.pipelinePOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinePocApplication.class, args);
+	}
+
+}
